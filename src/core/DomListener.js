e17ecdb5a6f930fd10b213e52d2e5e05f5435d48
Vitalii Sosluik <soslukven@gmail.com>
2020-09-10 23:20:15 +0300

    Added event listeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -7,8 +9,26 @@ export class DomListener {
     this.listeners = listeners;
   }
   initDOMListeners() {
-    console.log(this.listeners);
+    // console.log(this.listeners, this.$root);
+    this.listeners.forEach((listener) => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(
+          `Method ${method} is not implemented in ${name} Component`
+        );
+      }
+      console.log(this);
+      // Тоже самое что и addEventListener
+      this.$root.on(listener, this[method].bind(this));
+    });
   }
 
   removeDOMListeners() {}
 }
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName);
+}
+
+// input => onInput
--- src/core/dom.js
+++ src/core/dom.js
@@ -20,7 +20,9 @@ class Dom {
     return this;
   }
 
-  on() {}
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback);
+  }
 
   append(node) {
     if (node instanceof Dom) {
