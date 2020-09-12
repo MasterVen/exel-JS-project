71c100de3fd7e363fcda43b7c3d9f244310b48ca
Vitalii Sosluik <soslukven@gmail.com>
2020-09-12 16:21:53 +0300

    Created table, cells

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,9 +3,9 @@ const CODES = {
   Z: 90,
 };
 
-function createCell() {
+function toCell() {
   return `
-   <div class="cell" contenteditable="">B2</div>
+   <div class="cell" contenteditable=""></div>
   `;
 }
 
@@ -17,10 +17,10 @@ function toColumn(el) {
   `;
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
       <div class="row">
-        <div class="row-info"></div>
+        <div class="row-info">${index ? index : ''}</div>
         <div class="row-data">${content}</div>
       </div>
   `;
@@ -36,10 +36,11 @@ export function createTable(rowsCount = 15) {
 
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
   }
 
   return rows.join('');
